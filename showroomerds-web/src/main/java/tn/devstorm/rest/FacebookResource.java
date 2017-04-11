521d3eb9121d6b95ac5f23e1d337546fc5d717f5
Marouane-A <marouane.allagui@esprit.tn>
2017-04-11 17:17:01 +0100

    response type json for fb

--- showroomerds-web/src/main/java/tn/devstorm/rest/FacebookResource.java
+++ showroomerds-web/src/main/java/tn/devstorm/rest/FacebookResource.java
@@ -2,6 +2,8 @@ package tn.devstorm.rest;
 
 import javax.enterprise.context.RequestScoped;
 import javax.inject.Inject;
+import javax.json.Json;
+import javax.json.JsonObject;
 import javax.ws.rs.Consumes;
 import javax.ws.rs.GET;
 
@@ -26,8 +28,7 @@ public class FacebookResource {
 	@Path("/post/{prodId}")
 	@Consumes(MediaType.APPLICATION_JSON)
 	public String fbPost(@PathParam("prodId")int prodId){
-		return ejbfb.fbpostproduct(prodId);
-		
+		return ejbfb.fbpostproduct(prodId);		
 	}
 	@GET
 	@Path("/com/{id}/{text}")
@@ -37,13 +38,28 @@ public class FacebookResource {
 		return Response.status(Status.OK).build();			
 	}
 	@GET
-	@Path("/interaction/{prodId}")
-	@Consumes(MediaType.APPLICATION_JSON)
-	public String fbgetInteraction(@PathParam("prodId")int prodId){
+	@Path("/comments/{prodId}")
+	@Produces(MediaType.APPLICATION_JSON)
+	public Response fbgetCommentCount(@PathParam("prodId")int prodId){
 		long x= ejbfb.getCountComments(prodId);
-		long y=  ejbfb.getCountLikes(prodId);
-		long z=  ejbfb.getCountShares(prodId);
-		return "Comments = "+x+" Likes = "+y+"Shares = "+z;
+		 JsonObject json = Json.createObjectBuilder().add("comment_count", x).build();		 
+		 return Response.ok(json).build();
+	}
+	@GET
+	@Path("/likes/{prodId}")
+	@Produces(MediaType.APPLICATION_JSON)
+	public Response fbgetLikesCount(@PathParam("prodId")int prodId){
+		long x= ejbfb.getCountLikes(prodId);
+		JsonObject json = Json.createObjectBuilder().add("like_count", x).build();		 
+		 return Response.ok(json).build();
+	}
+	@GET
+	@Path("/shares/{prodId}")
+	@Produces(MediaType.APPLICATION_JSON)
+	public Response fbgetsharesCount(@PathParam("prodId")int prodId){
+		long x=ejbfb.getCountShares(prodId);
+		JsonObject json = Json.createObjectBuilder().add("share_count", x).build();		 
+		 return Response.ok(json).build();
 	}
 	
 
